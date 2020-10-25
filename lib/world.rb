690c8edad9d0446620c48be2470aa8036c24f3f4
Edward Andress <eddie_andress@hotmail.com>
2020-10-25 17:54:21 +0000

    refactor of world to 2d array

--- lib/world.rb
+++ lib/world.rb
@@ -1,33 +1,49 @@
+# the world doesn't decide who lives or dies
+# but it knows who is alive and who is dead
+# and it can carry out the deeds of a higher power
+
 class World
 
   def initialize(width, height, seed_indices = [])
-    @cells = Array.new(width * height, '•')
+    @cells = create_cells(width, height)
     add_seeds(seed_indices) if seed_indices.any?
   end
 
+  def create_cells(width, height)
+    (1..height).map do
+      (1..width).map do
+        '•'
+      end
+    end
+  end
+
   def live_cells
-    @cells.each_with_index.map do |cell, index|
-      index if alive?(cell)
-    end.compact
+    @cells.each_with_index.map do |row, row_index|
+      row.each_with_index.map do |cell, col_index|
+        [row_index, col_index] if alive?(cell)
+      end.compact
+    end.compact.flatten(1)
   end
 
   def dead_cells
-    @cells.each_with_index.map do |cell, index|
-      index if dead?(cell)
-    end.compact
+    @cells.each_with_index.map do |row, row_index|
+      row.each_with_index.map do |cell, col_index|
+        [row_index, col_index] if dead?(cell)
+      end.compact
+    end.compact.flatten(1)
   end
 
-  def animate_cell(index)
-    @cells[index] = ''
+  def animate_cell(row, col)
+    @cells[row][col] = ''
   end
 
-  def kill_cell(index)
-    @cells[index] = '•'
+  def kill_cell(row, col)
+    @cells[row][col] = '•'
   end
 
   def add_seeds(seed_indices)
-    seed_indices.each do |seed_index|
-      @cells[seed_index] = ''
+    seed_indices.each do |row, col|
+      @cells[row][col] = ''
     end
   end
 
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -2,47 +2,50 @@ require 'world'
 
 RSpec.describe World do
   context 'when the first cell of every row in a 3 x 3 grid is alive' do
-    subject { described_class.new(3, 3, [0,3,6]) }
+    let(:seeds) { [[0,0], [1,0], [2,0]] }
+    subject { described_class.new(3, 3, seeds) }
     
     describe '#live_cells' do
-      it 'returns the indices of all the live cells in the world' do
-        expect(subject.live_cells).to eq [0,3,6]
+      it 'returns the coordinates of all the live cells in the world' do
+        expect(subject.live_cells).to eq seeds
       end
     end
 
     describe '#dead_cells' do
-      it 'returns the indices of all the dead cells in the world' do
-        expect(subject.dead_cells).to eq [1,2,4,5,7,8]
+      it 'returns the coordinates of all the dead cells in the world' do
+        expect(subject.dead_cells)
+          .to eq [[0, 1], [0, 2], [1, 1], [1, 2], [2, 1], [2, 2]]
       end
     end
 
     describe '#kill_cell' do
 
       before do
-        subject.kill_cell(0)
+        subject.kill_cell(0,0)
       end
 
       it 'one fewer cells are alive' do
-        expect(subject.live_cells).to eq [3,6]
+        expect(subject.live_cells).to eq [[1, 0], [2, 0]]
       end
 
       it 'one more cell is dead' do
-        expect(subject.dead_cells).to eq [0,1,2,4,5,7,8]
+        expect(subject.dead_cells)
+          .to eq [[0, 0], [0, 1], [0, 2], [1, 1], [1, 2], [2, 1], [2, 2]]
       end
     end
 
     describe '#animate_cell' do
 
       before do
-        subject.animate_cell(1)
+        subject.animate_cell(0,1)
       end
 
       it 'one more cell is alive' do
-        expect(subject.live_cells).to eq [0,1,3,6]
+        expect(subject.live_cells).to eq [[0, 0], [0, 1], [1, 0], [2, 0]]
       end
 
       it 'one fewer cells are dead' do
-        expect(subject.dead_cells).to eq [2,4,5,7,8]
+        expect(subject.dead_cells).to eq [[0, 2], [1, 1], [1, 2], [2, 1], [2, 2]]
       end
     end
   end
