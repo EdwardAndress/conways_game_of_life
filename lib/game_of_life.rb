77c4b360ca1e9855eea700fa7d9e17eba477290c
Edward Andress <eddie_andress@hotmail.com>
2020-11-01 21:14:04 +0000

    adjust height in starter script

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,7 +1,7 @@
 require_relative './game_runner'
 
 WIDTH = 100
-HEIGHT = 50
+HEIGHT = 40
 GENERATIONS = 100
 
 seeds = []
@@ -10,6 +10,6 @@ seeds = []
   seeds << [rand(HEIGHT - 1), rand(WIDTH - 1)]
 end
 
-world = World.new(seeds: seeds)
+world = World.new(seeds: seeds, height: HEIGHT, width: WIDTH)
 game_runner = GameRunner.new(world: world)
 game_runner.run
