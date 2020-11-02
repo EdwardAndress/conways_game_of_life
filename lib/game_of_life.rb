ca90cfe0e28d9461e0feafce11b15aefbba76ad1
Edward Andress <eddie_andress@hotmail.com>
2020-11-02 21:34:16 +0000

    now with glider gun

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,15 +1,16 @@
 require_relative './game_runner'
+require_relative './glider_gun'
 
 WIDTH = 100
 HEIGHT = 40
 GENERATIONS = 100
 
-seeds = []
+# seeds = []
 
-((WIDTH * HEIGHT) / 10).times do
-  seeds << [rand(HEIGHT - 1), rand(WIDTH - 1)]
-end
+# ((WIDTH * HEIGHT) / 10).times do
+#   seeds << [rand(HEIGHT - 1), rand(WIDTH - 1)]
+# end
 
-world = World.new(seeds: seeds, height: HEIGHT, width: WIDTH)
+world = World.new(seeds: glider_gun, height: HEIGHT, width: WIDTH)
 game_runner = GameRunner.new(world: world)
 game_runner.run
